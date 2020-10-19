b54eb7c336a0babdb6bcf0a20043ea45ae1ab11c
arumayara <72974847+arumayara@users.noreply.github.com>
2020-10-19 07:10:21 +0700

    Add files via upload

---
+++ PBO10K-10119905-Latihan1-Biodata/src/main/java/PBO10K/NIM10119905/Latihan1/Biodata/PBO10K10119905Latihan1.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO10K.NIM10119905.Latihan1.Biodata;
+
+/**
+ *
+ * @author
+ * NAMA    : Abraham Rumayara
+ * KELAS   : PBO10K
+ * NIM     : 10119905
+ * Deskripsi Program : Program ini merupakan perintah menampilkan value ke layar
+ * 
+ */
+public class PBO10K10119905Latihan1 {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "----------------------");
+        System.out.println("NAMA\t\t: Abraham Rumayara");
+        System.out.println("ALAMAT\t\t: RT.28 RW.10 Cigugur, Kuningan");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 8 Juli 1998");
+        System.out.println("----------------------");
+        System.out.println("Hobi\t\t: Renang, Masak & Jalan-jalan");
+        System.out.println("MAKES\t\t: Cheese Cake");
+        System.out.println("MIKES\t\t: Boba fresh milk");
+         
+    }
+    
+}
